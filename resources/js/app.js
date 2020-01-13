90fd871b8802c54ee8ce9ac2a89e27b3408e79fe
Charlie <charleschengcy94@gmail.com>
2020-01-13 05:10:40 +0000

    修改和删除收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
+import Vue from 'vue';
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -26,7 +26,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-import Vue from 'vue';
+
 require('./components/SelectDistrict');
 require('./components/UserAddressesCreateAndEdit');
 
